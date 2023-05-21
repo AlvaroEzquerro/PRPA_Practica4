655cc3b793c8d16882ceea2b70cf801534eb7b22
Alvaro Ezquerro <alvaroez@ucm.es>
2023-05-21 19:37:55 +0200

    primeras 5 preguntas

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ La práctica consiste en el diseño e implementación de una solución a un prob
 ##Dataset
 El dataset sobre el que vamos a trabajar es el de BICIMAD. Todo lo referente a estos datos se puede encontrar en https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=d67921bb86e64610VgnVCM2000001f4a900aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD&vgnextfmt=default
 
-Los archivos son formato json y son de la siguiente forma:
+Los archivos son formato json y son contienen los siguientes datos(Para una explicación más detallada ver Servicios-y-estructuras-Bicimad-V1-1.pdf):
 
 - Tipo de usuario: cliente o personal de mantenimiento.
 - Código de usuario.
@@ -18,17 +18,21 @@ Los archivos son formato json y son de la siguiente forma:
 - Número de la base de la que se desengancha la bicicleta.
 - Número de la base en la que se engancha la bicicleta.
 - Tiempo transcurrido entre el enganche y el desenganche de la bicicleta.
-- Hora a la que se realiza el desenganche de la bicicleta.
+- Hora a la que se realiza el desenganche de la bicicleta. (formato: "2017-09-22T14:00:00.000+0200")
+- Rango de edad del usuario.
+- Tipo de usuario: trabajador de empresa, usuario anual y usuario ocasional.
 
 
 A continuación, exponemos cada una de las problemáticas que nos hemos enfrentado y como hemos decidido abordarlas:
 
-## 1. ¿Cuáles son las estaciones más transitadas? ¿Y las menos?
+## 1. Los trayectos más realizados y los que menos.
 
+## 2. Optimizar las estaciones que hay, es decir, ver cuales son las menos utilizadas y las más. 
 
-## 2. ¿Cuál es el trayecto más repetido? ¿Y el menos?
-
-
-## 3. ¿Cuál es la hora a la que hay mayor número de usuarios activos? ¿Sería conveniente aumentar el personal de mantenimiento a esas horas?
+## 3. Determinar la hora punta de uso.
 
 ## 4. ¿Hay un gran número de clientes habituales, o sería recomendable plantear sistemas de fidelización para aumentar dicho número?
+
+## 5. Porcentajes de uso dependiendo del rango de edad y el tipo de usuario.
+
+## 6. Cantidad de bicicletas rotas, es decir, cuyo viaje consta de un tiempo bajo (<60 seg)
--- bicimad.py
+++ bicimad.py
@@ -1,34 +1,147 @@
 from pyspark import SparkContext
+from datetime import datetime
 import json #Sirve para leer el formato en el qeu estan las bases de datos
 
 
 def mapper(line): #Funcion para extraer los datos de cada una de las lineas del archivo anterior
+    '''
+    user_age:
+        0: No se ha podido determinar el rango de edad del usuario
+        1: El usuario tiene entre 0 y 16 años
+        2: El usuario tiene entre 17 y 18 años
+        3: El usuario tiene entre 19 y 26 años
+        4: El usuario tiene entre 27 y 40 años
+        5: El usuario tiene entre 41 y 65 años
+        6: El usuario tiene 66 años o más
+
+    user_type:
+        0: No se ha podido determinar el tipo de usuario
+        1: Usuario anual (poseedor de un pase anual)
+        2: Usuario ocasional
+        3: Trabajador de la empresa
+    '''
     data = json.loads(line)
     user_type = data['user_type']
+    user_age = data['ageRange']
     user_day_code = data['user_day_code']
     start_station = data['idunplug_station']
     end_station = data['idplug_station']
-    time = data['travel_time']
-    return user_type, user_day_code, start_station, end_station, time
-
-def ruta_mas_repetida(rdd):
-    ruta, veces = rdd.map(lambda x: ((x[2], x[3]),1)).\
+    duration = data['travel_time']
+    date = datetime.strptime(data["unplug_hourTime"]['$date'][:-4], '%Y-%m-%dT%H:%M:%S.000+')
+    return user_type, user_day_code, start_station, end_station, duration, date, user_age
+'''
+Indices:
+    0-user_type
+    1-user_day_code
+    2-start_station
+    3-end_station
+    4-time
+    5-date
+    6-user_age
+    
+'''
+user_types = {0:'Sin datos', 1:'Usuario anual', 2:'Usuario ocasional',\
+              3:'Trabajador de empresa'}
+user_ages = {0: 'Sin datos',\
+             1: '<17',\
+             2: '17-18',\
+             3: '19-26',\
+             4: '27-40',\
+             5: '41-65',\
+             6: '>65'}
+    
+def rutas_ordenadas(rdd):
+    rutas_ordenadas = rdd.map(lambda x: ((x[2], x[3]),1)).\
         reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = False).first()       
-    return ruta, veces
+        sortBy(lambda x: x[1], ascending = False)     
+    return rutas_ordenadas
 
-def rutas_hechas_1_vez(rdd):
-    pass
+def estaciones_ordenadas(rdd):
+    Estaciones = rdd.flatMap(lambda x: [(x[2], 1), (x[3], 1)]).\
+        reduceByKey(lambda x, y: x+y).\
+        sortBy(lambda x: x[1], ascending = False)
+    estacioneS = rdd.flatMap(lambda x: [(x[2], 1), (x[3], 1)]).\
+        reduceByKey(lambda x, y: x+y).\
+        sortBy(lambda x: x[1], ascending = True)
+    return Estaciones, estacioneS
+
+def horas_ordenadas(rdd):
+    horas = rdd.map(lambda x: (x[5].hour, 1)).\
+        reduceByKey(lambda x, y: x+y).\
+        sortBy(lambda x: x[1], ascending = False)
+    total = horas.map(lambda x: x[1]).sum() #total de viajes
+    horas = horas.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
+    return horas
+
+def tipos_ordenados(rdd):
+    tipos = rdd.map(lambda x: (user_types[x[0]], 1)).\
+        reduceByKey(lambda x, y: x+y).\
+        sortBy(lambda x: x[1], ascending = False)
+    total = tipos.map(lambda x: x[1]).sum() #total de viajes
+    tipos = tipos.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
+    return tipos
+
+def edades_ordenadas(rdd):
+    edades = rdd.map(lambda x: (user_ages[x[0]], 1)).\
+        reduceByKey(lambda x, y: x+y).\
+        sortBy(lambda x: x[1], ascending = False)
+    total = edades.map(lambda x: x[1]).sum() #total de viajes
+    edades = edades.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
+    return edades
 
 def main():
     with SparkContext() as sc:
         sc.setLogLevel("ERROR")
         
-        rdd_base = sc.textFile('sample_10e3.json') #Aqui leemos el archivo .json
+        rdd_base = sc.textFile('sample_10e4.json') #Aqui leemos el archivo .json
         rdd = rdd_base.map(mapper) #Aplicamos la funcion anterior a todo el archivo
-        ruta, veces = ruta_mas_repetida(rdd)
-        print('La ruta mas repetida es:', ruta)
-        print('Realizada', veces, 'veces')
-
+        n = 5
+        
+        print('------------------------------1-------------------------------')
+        
+        rutas = rutas_ordenadas(rdd)
+        print('Las', n, 'rutas mas repetidas son:')
+        print(rutas.map(lambda x: x[0]).take(n))
+        print('realizadas cada una este numero de veces:')
+        print(rutas.map(lambda x: x[1]).take(n))
+        print('Además, las siguientes rutas solo se realizan una vez:')
+        #print(rutas.filter(lambda x: x[1] == 1).map(lambda x: x[0]).collect())
+        
+        print('------------------------------2-------------------------------')
+        
+        Estaciones, estacioneS = estaciones_ordenadas(rdd)
+        print('Estas son las', n, 'estaciones más transitadas:')
+        print(Estaciones.map(lambda x: x[0]).take(n))
+        print('con este numero de usos cada una:')
+        print(Estaciones.map(lambda x: x[1]).take(n))
+        print('Además, estas son las', n, ' estaciones menos transitadas:')
+        print(estacioneS.map(lambda x: x[0]).take(n))
+        print('con este numero de usos cada una:')
+        print(estacioneS.map(lambda x: x[1]).take(n))
+        
+        print('------------------------------3-------------------------------')
+        
+        horas = horas_ordenadas(rdd)
+        print('Las horas ordenadas en cuanto a mayor uso son:')
+        print(horas.map(lambda x: x[0]).collect())
+        print('con porcentajes:')
+        print(horas.map(lambda x: x[1]).collect())
+        
+        print('------------------------------4-------------------------------')
+        
+        
+        
+        print('------------------------------5-------------------------------')
+        
+        edades = edades_ordenadas(rdd)
+        print('Las edades de los usuarios ordenadas en cuanto a más uso son:')
+        print(edades.map(lambda x: x[0]).collect())
+        print('con porcentajes:')
+        print(edades.map(lambda x: x[1]).collect())
+        
+        print('------------------------------6-------------------------------')
+        
+        
+        
 if __name__ == '__main__':
     main()

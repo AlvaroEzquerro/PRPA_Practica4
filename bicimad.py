c24925595c87a5bed4c4e96dcee1b7b1a4a2afd7
Alvaro Ezquerro <alvaroez@ucm.es>
2023-05-26 17:43:19 +0200

    Solucionar problemas, reordenar codigo y pregunta 6

--- bicimad.py
+++ bicimad.py
@@ -31,67 +31,33 @@ def mapper(line): #Funcion para extraer los datos de cada una de las lineas del 
     date = datetime.strptime(data["unplug_hourTime"], '%Y-%m-%dT%H:%M:%SZ')
     return user_type, user_day_code, start_station, end_station, duration, date, user_age
     # No sería más comodo devolver esto como un diccionario o un objeto?
+    
+    # Alvaro:
+    # Piensa que se hace .map(mapper), entonces por cada dato creariamos un dict
 '''
 Indices:
     0-user_type
     1-user_day_code
     2-start_station
     3-end_station
-    4-time
+    4-duration
     5-date
     6-user_age
     
 '''
-user_types = {0:'Sin datos', 1:'Usuario anual', 2:'Usuario ocasional',\
+user_types = {0:'NaN', 6:'NaN', 7:'NaN', 1:'Usuario anual', 2:'Usuario ocasional',\
               3:'Trabajador de empresa'}
-user_ages = {0: 'Sin datos',\
+    
+user_ages = {0: 'NaN',\
              1: '<17',\
              2: '17-18',\
              3: '19-26',\
              4: '27-40',\
              5: '41-65',\
              6: '>65'}
-    
-def rutas_ordenadas(rdd):
-    rutas_ordenadas = rdd.map(lambda x: ((x[2], x[3]),1)).\
-        reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = False)     
-    return rutas_ordenadas
 
-def estaciones_ordenadas(rdd):
-    Estaciones = rdd.flatMap(lambda x: [(x[2], 1), (x[3], 1)]).\
-        reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = False)
-    estacioneS = rdd.flatMap(lambda x: [(x[2], 1), (x[3], 1)]).\
-        reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = True)
-    return Estaciones, estacioneS
 
-def horas_ordenadas(rdd):
-    horas = rdd.map(lambda x: (x[5].hour, 1)).\
-        reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = False)
-    total = horas.map(lambda x: x[1]).sum() #total de viajes
-    horas = horas.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
-    return horas
-
-def tipos_ordenados(rdd):
-    tipos = rdd.map(lambda x: (user_types[x[0]], 1)).\
-        reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = False)
-    total = tipos.map(lambda x: x[1]).sum() #total de viajes
-    tipos = tipos.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
-    return tipos
-
-def edades_ordenadas(rdd):
-    edades = rdd.map(lambda x: (user_ages[x[0]], 1)).\
-        reduceByKey(lambda x, y: x+y).\
-        sortBy(lambda x: x[1], ascending = False)
-    total = edades.map(lambda x: x[1]).sum() #total de viajes
-    edades = edades.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
-    return edades
-
- # Usamos una tupla (inmutable) como llave
+# Usamos una tupla (inmutable) como llave
 def trayectos_habituales(rdd):
     trayectos_ordenados = rdd.map(lambda x: (x[2],x[3]) ).\
                               countByValue().\
@@ -119,8 +85,107 @@ def clientes_habituales(rdd, margen):
     repeticiones_horario = rdd.map(lambda x: ((x[2],x[3], x[5].hour, x[6]) , x[4]) ).\
                            aggregateByKey(to_listas, lambda a, b : combinar_si_margen(a,b,margen)).\
                            flatMap(lambda x: x[1]).map(lambda x: x[1]).sort()
-    return repeticiones_horario                             # Devuelve el numero de veces que se ha repetido cada trayecto por una misma persona                                                             
-                                                             
+    return repeticiones_horario# Devuelve el numero de veces que se ha repetido cada trayecto por una misma persona                                                             
+
+def pregunta1(rdd, n):
+    
+    def rutas_ordenadas(rdd):
+        rutas_ordenadas = rdd.map(lambda x: ((x[2], x[3]),1)).\
+            reduceByKey(lambda x, y: x+y).\
+            sortBy(lambda x: x[1], ascending = False)     
+        return rutas_ordenadas
+    
+    print('------------------------------1-------------------------------')
+    
+    rutas = rutas_ordenadas(rdd)
+    print('Las', n, 'rutas mas repetidas son:')
+    print(rutas.map(lambda x: x[0]).take(n))
+    print('realizadas cada una este numero de veces:')
+    print(rutas.map(lambda x: x[1]).take(n))
+    
+def pregunta2(rdd, n):
+    
+    def estaciones_ordenadas(rdd):
+        Estaciones = rdd.flatMap(lambda x: [(x[2], 1), (x[3], 1)]).\
+            reduceByKey(lambda x, y: x+y).\
+            sortBy(lambda x: x[1], ascending = False)
+        estacioneS = rdd.flatMap(lambda x: [(x[2], 1), (x[3], 1)]).\
+            reduceByKey(lambda x, y: x+y).\
+            sortBy(lambda x: x[1], ascending = True)
+        return Estaciones, estacioneS
+    
+    print('------------------------------2-------------------------------')
+    
+    Estaciones, estacioneS = estaciones_ordenadas(rdd)
+    print('Estas son las', n, 'estaciones más transitadas:')
+    print(Estaciones.map(lambda x: x[0]).take(n))
+    print('con este numero de usos cada una:')
+    print(Estaciones.map(lambda x: x[1]).take(n))
+    print('Además, estas son las', n, ' estaciones menos transitadas:')
+    print(estacioneS.map(lambda x: x[0]).take(n))
+    print('con este numero de usos cada una:')
+    print(estacioneS.map(lambda x: x[1]).take(n))
+    
+def pregunta3(rdd, n):
+    
+    def horas_ordenadas(rdd):
+        horas = rdd.map(lambda x: (x[5].hour, 1)).\
+            reduceByKey(lambda x, y: x+y).\
+            sortBy(lambda x: x[1], ascending = False)
+        total = horas.map(lambda x: x[1]).sum() #total de viajes
+        horas = horas.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
+        return horas
+    
+    print('------------------------------3-------------------------------')
+    
+    horas = horas_ordenadas(rdd)
+    print('Las horas ordenadas en cuanto a mayor uso son:')
+    print(horas.map(lambda x: x[0]).take(n))
+    print('con porcentajes:')
+    print(horas.map(lambda x: round(x[1], 4)).take(n))
+
+def pregunta5(rdd, n):
+    
+    def edades_ordenadas(rdd):
+        edades = rdd.map(lambda x: (user_ages[x[6]], 1)).\
+            reduceByKey(lambda x, y: x+y).\
+            sortBy(lambda x: x[1], ascending = False)
+        total = edades.map(lambda x: x[1]).sum() #total de viajes
+        edades = edades.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
+        return edades
+    
+    def tipos_ordenados(rdd):
+        tipos = rdd.map(lambda x: (user_types[x[0]], 1)).\
+            reduceByKey(lambda x, y: x+y).\
+            sortBy(lambda x: x[1], ascending = False)
+        total = tipos.map(lambda x: x[1]).sum() #total de viajes
+        tipos = tipos.map(lambda x: (x[0], x[1]*100/total)) #cambio el total por porcentajes
+        return tipos
+    
+    print('------------------------------5-------------------------------')
+    
+    edades = edades_ordenadas(rdd)
+    print('Las edades de los usuarios ordenadas en cuanto a más uso son:')
+    print(edades.map(lambda x: x[0]).collect())
+    print('con porcentajes:')
+    print(edades.map(lambda x: round(x[1], 4)).collect())
+    
+    tipos = tipos_ordenados(rdd)
+    print('Los tipos de usuario ordenadas en cuanto a más uso son:')
+    print(tipos.map(lambda x: x[0]).collect())
+    print('con porcentajes:')
+    print(tipos.map(lambda x: round(x[1], 4)).collect())
+    
+def pregunta6(rdd, n):
+    print('------------------------------6-------------------------------')
+    
+    total = rdd.count()
+    rotas = rdd.filter(lambda x: x[4]<60).count()
+    print('Este es el número de veces que alguien ha cogido una bici rota:')
+    print(rotas)
+    print('de un total de viajes de:')
+    print(total, '('+str(round(rotas/total, 4)),'%)')                 
+                                           
 def main(dataset):
     with SparkContext() as sc:
         sc.setLogLevel("ERROR")
@@ -129,54 +194,21 @@ def main(dataset):
         rdd = rdd_base.map(mapper) #Aplicamos la funcion anterior a todo el archivo
         n = 5
         
-        print('------------------------------1-------------------------------')
+        pregunta1(rdd, n)
         
-        rutas = rutas_ordenadas(rdd)
-        print('Las', n, 'rutas mas repetidas son:')
-        print(rutas.map(lambda x: x[0]).take(n))
-        print('realizadas cada una este numero de veces:')
-        print(rutas.map(lambda x: x[1]).take(n))
-        print('Además, las siguientes rutas solo se realizan una vez:')
-        #print(rutas.filter(lambda x: x[1] == 1).map(lambda x: x[0]).collect())
+        pregunta2(rdd, n)
         
-        print('------------------------------2-------------------------------')
+        pregunta3(rdd, n)
         
-        Estaciones, estacioneS = estaciones_ordenadas(rdd)
-        print('Estas son las', n, 'estaciones más transitadas:')
-        print(Estaciones.map(lambda x: x[0]).take(n))
-        print('con este numero de usos cada una:')
-        print(Estaciones.map(lambda x: x[1]).take(n))
-        print('Además, estas son las', n, ' estaciones menos transitadas:')
-        print(estacioneS.map(lambda x: x[0]).take(n))
-        print('con este numero de usos cada una:')
-        print(estacioneS.map(lambda x: x[1]).take(n))
+        pregunta5(rdd, n)
         
-        print('------------------------------3-------------------------------')
+        pregunta6(rdd, n)    
         
-        horas = horas_ordenadas(rdd)
-        print('Las horas ordenadas en cuanto a mayor uso son:')
-        print(horas.map(lambda x: x[0]).take(n))
-        print('con porcentajes:')
-        print(horas.map(lambda x: x[1]).take(n))
-        
-        print('------------------------------4-------------------------------')
-        
-        
-        
-        print('------------------------------5-------------------------------')
-        
-        edades = edades_ordenadas(rdd)
-        print('Las edades de los usuarios ordenadas en cuanto a más uso son:')
-        print(edades.map(lambda x: x[0]).collect())
-        print('con porcentajes:')
-        print(edades.map(lambda x: x[1]).collect())
-        
-        print('------------------------------6-------------------------------')
         
         
         
 if __name__ == '__main__':
     if len(sys.argv)>1:
         dataset = sys.argv[1]
-    main(dataset)
-    #main('sample_10e2.json')
+    #main(dataset)
+    main('202103_movements.json')
